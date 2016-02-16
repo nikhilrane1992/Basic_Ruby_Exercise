13a072b30ef0c3fe55ea5324fadb988c6d1f115d
Nikhil Rane <nikhilrane1992@gamil.com>
2016-02-16 15:20:55 +0530

    Working with numbers

---
+++ Ruby_Projects/working_with_number.rb
@@ -0,0 +1,54 @@
+## Addition
+puts 1 + 1
+
+## Subtraction
+puts 1-1
+
+## Multiplication
+puts 3*5
+
+## Division
+puts 20/3
+puts 20/3.0
+puts 20/3.to_f
+
+## Reminder
+puts 20 % 3
+
+## check odd or even
+puts 20.odd?
+puts 20.even?
+
+## compare number
+puts 20 < 10
+puts 20 > 10
+puts 20 >= 10
+puts 20 <= 10
+puts 20 == 10
+
+## generate random number
+puts rand
+puts rand(10)
+puts rand(10)
+
+## convert float to integer and integer to float
+x = 15
+y = 20.56
+puts x.to_f
+puts y.to_i
+
+## convert string to int and int to string
+x = "5"
+y = 5
+
+puts x.to_i
+puts y.to_s
+
+## multiply string to number
+puts "Nikhil "*3
+
+## Print string Hi in 20 times
+20.times {puts "Hi"}
+
+## Print int 24 in 20 times
+20.times {puts 24}
